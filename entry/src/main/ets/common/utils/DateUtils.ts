2e3be699d2bee80814a3c7af98d8a2867a4fb9cb
TANGhz17 <15851238039@163.com>
2024-03-07 23:08:18 +0800

    获取的数组没问题，应该是Grid渲染是只保留了一个空的，可能是Grid的自动去除相同的功能

--- entry/src/main/ets/common/utils/DateUtils.ts
+++ entry/src/main/ets/common/utils/DateUtils.ts
@@ -3,7 +3,7 @@ export default class DateUtils {
    * 获取一个月的第一天是周几
    */
   static getFirstDayOfMonthInWeek(year, month) {
-    return new Date(year, month, 1).getDay();
+    return new Date(year, month, 1).getUTCDay();
   }
 
   static getDaysOfMonth(year, month) {
